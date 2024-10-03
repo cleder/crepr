38b190a4f60f0763ed66b5f94c1ab0435c68b92a
Soham Mhatre <sohammhatre521@gmail.com>
2024-10-03 16:07:23 +0530

    Update class_with_repr_test.py
    Signed-off-by: Soham Mhatre <sohammhatre521@gmail.com>

--- tests/classes/class_with_repr_test.py
+++ tests/classes/class_with_repr_test.py
@@ -1,14 +1,22 @@
 """Module containing class definitions for testing purposes.
 
-This module defines a class without a `__repr__` method.
+This module defines a class with a `__repr__` method.
 """
 
 from typing import Self
 
 
-class MyClassWithoutRepr:
+class MyClassWithRepr:
     """A class without a `__repr__` method."""
 
     def __init__(self: Self, value: str) -> None:
         """Initialize the class with the given value."""
         self.value = value
+
+    def __repr__(self: Self) -> str:
+        """Create a string (c)representation for MyClassWithRepr."""
+        return (
+            f"{self.__class__.__module__}.{self.__class__.__name__}("
+            f"value={self.value!r}, "
+            ")"
+        )
