ef381220299bdf2a00999199ebc63a50c2000cef
Christian Ledermann <christian.ledermann@gmail.com>
2024-10-09 12:05:32 +0100

    Refactor check_version_tag.py to handle missing file name argument

--- check_version_tag.py
+++ check_version_tag.py
@@ -3,15 +3,13 @@
 
 Usage: check_version_tag.py <filename> <tag_version>
 
-This script is used to check if the version in the about.py file matches the
-given version. The version in the about.py file is expected to be a string
-assigned to the __version__ attribute of the module. The given version is
-expected to be a PEP-386 compliant version string.
+This script checks if the version in the project metadata matches the given version.
+
 """
-import importlib.util
+import argparse
 import re
 import sys
-from typing import cast
+from pathlib import Path
 
 pattern = (
     r"^(?P<version>\d+\.\d+)(?P<extraversion>(?:\.\d+)*)"
@@ -20,40 +18,61 @@ pattern = (
 )
 
 
-def get_version_from_module(file_name: str) -> str:
-    """Get the version from the module."""
+def guess_file_name() -> Path:
+    """Guesses the file name of the PKG-INFO file within an .egg-info directory.
+
+    This function searches for directories with the .egg-info extension in the current
+    directory. If such a directory is found, it returns the path to the PKG-INFO file
+    within that directory.
+    If no .egg-info directory is found, it writes an error message to stdout and
+    exits the program with a status code of 1.
+
+    Returns:
+        Path: The path to the PKG-INFO file within the found .egg-info directory.
+
+    Raises:
+        SystemExit: If no .egg-info directory is found.
+
+    """
+    """"""
+    egg_info_dirs = list(Path().glob("*.egg-info"))
+    if egg_info_dirs:
+        return egg_info_dirs[0] / "PKG-INFO"
+    sys.stdout.write("No filename provided and no '*.egg-info' directory found\n")
+    sys.exit(1)
+
+
+def get_version_from_pkg_info(file_name: str) -> str:
+    """Get the version from the file."""
+    file_path = Path(file_name)
+    if not file_name:
+        file_path = guess_file_name()
     try:
-        spec = importlib.util.spec_from_file_location("module.name", file_name)
-        assert spec is not None  # noqa: S101
-        module = importlib.util.module_from_spec(spec)
-        assert spec.loader is not None  # noqa: S101
-        spec.loader.exec_module(module)
+        with file_path.open("r") as f:
+            for line in f:
+                if line.startswith("Version:"):
+                    return line.split(":")[1].strip()
+                if line.startswith("__version__"):
+                    return line.split("=")[1].strip()
     except Exception as e:  # noqa: BLE001
-        sys.stdout.write(f"Error loading file {file_name}: {e}")
+        sys.stdout.write(f"Error loading file {file_name}: {e}\n")
         sys.exit(1)
-    try:
-        version = module.__version__
-        if not isinstance(version, str):
-            sys.stdout.write(f"Version in {file_name} is not a string")
-            sys.exit(1)
-        return cast(str, module.__version__)
-    except AttributeError:
-        sys.stdout.write(f"Module {file_name} has no __version__ attribute")
-        sys.exit(1)
+    sys.stdout.write(f"No Version found in '{file_name}'\n")
+    sys.exit(1)
 
 
-def main(filename: str, tag_name: str) -> None:
+def main(file_name: str, tag_name: str) -> None:
     """Check if the version in the filename file matches the given version."""
-    version = get_version_from_module(filename)
+    version = get_version_from_pkg_info(file_name)
     errors = False
     if version != tag_name:
-        sys.stdout.write(f"Version {version} does not match tag {tag_name}")
+        sys.stdout.write(f"Version {version} does not match tag {tag_name}\n")
         errors = True
     if not re.match(pattern, tag_name):
-        sys.stdout.write(f"Tag name '{tag_name}' is not PEP-386 compliant")
+        sys.stdout.write(f"Tag name '{tag_name}' is not PEP-386 compliant\n")
         errors = True
     if not re.match(pattern, version):
-        sys.stdout.write(f"Version {version} is not PEP-386 compliant")
+        sys.stdout.write(f"Version {version} is not PEP-386 compliant\n")
         errors = True
     if errors:
         sys.exit(1)
@@ -61,10 +80,18 @@ def main(filename: str, tag_name: str) -> None:
 
 
 if __name__ == "__main__":
-    try:
-        file_name = sys.argv[1]
-        tag_name = sys.argv[2]
-    except IndexError:
-        sys.stdout.write("Usage: check_version_tag.py <filename> <version>")
-        sys.exit(1)
-    main(file_name, tag_name)
+
+    parser = argparse.ArgumentParser(
+        description="Check if the version in the metadata matches the given version.",
+    )
+    parser.add_argument("tag_version", help="The version tag to compare against.")
+    parser.add_argument(
+        "filename",
+        nargs="?",
+        default="",
+        help="The path to the file containing the version information.",
+    )
+
+    args = parser.parse_args()
+
+    main(args.filename, args.tag_version)
